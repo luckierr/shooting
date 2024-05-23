10bdbb94232a0ef7cd9a31048e9ff0baff8f5c23
mpanibratska <26958223-mpanibratska@users.noreply.replit.com>
2024-05-23 11:02:13 +0000

    1 2 3

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+var screen = "MENU";
+
+class Target {
+  constructor(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+  }
+
+  draw() {
+    this.x += this.speed;
+
+    // перевірити чи ціль вилеліла за межі екрану
+    
+    circle(this.x, this.y, 50);
+  }
+}
+
+var targets = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+  createCanvas(800, 600);
+
+  targets.push(
+    new Target(random(width), random(height), random(1, 5))
+  );
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  if (screen == "MENU") {
+    background("lightblue");
+    text("START GAME", 300, 300, stroke(5), textSize(30));
+  }
+  if (screen == "GAME") {
+    background("lightblue");
+
+    for (var i = 0; i < targets.length; i++){
+      targets[i].draw();
+    } 
+  }
+}
+
+function mouseClicked() {
+  if (screen == "MENU") {
+    screen = "GAME";
+  }
+  if (screen == "GAME") {
+    // перевірити mouseX, mouseY на потрапляння в якусь ціль
+    // підрахунок балів
+  }
 }
